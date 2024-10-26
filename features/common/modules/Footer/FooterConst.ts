46e4ce7fe1d3bd19287f721161e8990e1fca7814
Jens Tofte <54096088+Jens-source@users.noreply.github.com>
2024-10-26 08:33:17 +0700

    sdfwefwe

--- features/common/modules/Footer/FooterConst.ts
+++ features/common/modules/Footer/FooterConst.ts
@@ -1,7 +1,7 @@
 export const services = [
   { name: 'Our Services', link: '/our-services' },
   { name: 'Meet The Team', link: '/meet-the-team' },
-  { name: 'Careers at Miror', link: '/careers-at-casaz' },
+  { name: 'Careers at Miror', link: '/careers-at-miror' },
   { name: 'Latest News & Videos', link: '/latest-news' },
 ];
 
